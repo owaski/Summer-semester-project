2baf6a5dbb206f0af6f20020f5bc0b93b323ba15
Owaski <owaski@Owaskis-MacBook-Pro.local>
2018-08-28 17:30:19 +0800

    The first version on git

---
+++ Paper/auto/project.el
@@ -0,0 +1,26 @@
+(TeX-add-style-hook
+ "project"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "aaai"
+    "times"
+    "helvet"
+    "courier"
+    "graphicx"
+    "csvsimple")
+   (LaTeX-add-labels
+    "figure:0"
+    "figure:1"
+    "table:1"
+    "figure:2"
+    "figure:3"
+    "figure:4"
+    "figure:5"
+    "figure:6"))
+ :latex)
+
