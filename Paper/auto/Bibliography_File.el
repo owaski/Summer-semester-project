84da2cc92661a1808f3581ac0feac0dc693a8c90
Owaski <owaski@Owaskis-MacBook-Pro.local>
2018-08-30 15:41:10 +0800

    add bibliography, introduction, conclusion together with some modifications

---
+++ Paper/auto/Bibliography_File.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "Bibliography_File"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "p1"
+    "p2"
+    "p3"
+    "p4"
+    "p5"
+    "wiki:1"
+    "wiki:2"))
+ :bibtex)
+
--- Paper/auto/project.el
+++ Paper/auto/project.el
@@ -5,6 +5,13 @@
                      '(("article" "letterpaper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("hyperref" "hidelinks") ("hypcap" "all")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -15,19 +22,23 @@
     "courier"
     "graphicx"
     "csvsimple"
+    "CJK"
+    "url"
     "hyperref"
     "hypcap")
    (LaTeX-add-labels
     "figure:0"
     "figure:1"
-    "table:1"
     "figure:2"
     "figure:3"
     "figure:4"
     "figure:5"
     "figure:6"
     "figure:7"
+    "table:1"
     "table:2"
-    "table:3"))
+    "table:3")
+   (LaTeX-add-bibliographies
+    "Bibliography_File"))
  :latex)
 
